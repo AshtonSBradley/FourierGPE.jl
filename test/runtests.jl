224e644d1066b2c1821a9510789824be9db4c134
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2019-04-23 10:06:45 +1200

    small beer

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using FourierGPE, Test
 
-#runtests
 @testset "Parseval test" begin include("parsevaltests.jl") end
-# @testset "Mixed Parseval tests" begin include("mixedparsevaltests.jl") end
+# @testset "Mixed Parseval" begin include("mixedparsevaltests.jl") end
 @testset "Ground state" begin include("groundstatetests.jl") end
 @testset "Dynamics" begin include("dynamicstests.jl") end
