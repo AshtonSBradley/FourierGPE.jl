f808e69a768160a74ebdd383b9a74e0eeceb9516
Ashton Bradley <ashton.bradley@gmail.com>
2019-07-17 15:57:19 +1200

    tests pass!

--- test/groundstatetests.jl
+++ test/groundstatetests.jl
@@ -2,7 +2,7 @@
 # using Test, Revise, FourierGPE
 
 import FourierGPE:V
-V(x,t) = 0.5*ω^2*x^2
+V(x,t) = 0.5*x^2
 
 
 # ==== set simulation parameters ====
@@ -19,11 +19,11 @@ sim = Sim(L,N)
 # useful TF state
 ψ0(x,μ,g) = sqrt(μ/g)*sqrt(max(1.0-V(x,0.0)/μ,0.0)+im*0.0)
 
-#evaluate on grid
+# evaluate on grid
 x = X[1]
 ψi = ψ0.(x,μ,g)
 
-#make initial state
+# make initial state
 ϕi = kspace(ψi,sim)
 @pack! sim = ϕi
 
