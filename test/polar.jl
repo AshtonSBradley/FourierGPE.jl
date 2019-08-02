0e6591d77fdf7f066d5b1199b9d029868227405a
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2019-08-03 08:22:44 +1200

    simplify Oscillator in polar

--- test/polar.jl
+++ test/polar.jl
@@ -1,63 +1,65 @@
 using Test, Plots, SpecialFunctions, FastGaussQuadrature, Parameters
+gr(aspect_ratio=1)
 
-N = 500
-xm = 5.
-x = LinRange(-xm,xm,N); y = x
+Nx = 500
+xm = 10.
+x = LinRange(-xm,xm,Nx); y = x
 psi = @. 10*exp(-.5*(x^2+y'^2)) + randn()+im*randn()
 heatmap(x,y,abs2.(psi))
 
-M = 20
-x,w = gausshermite(M)
-Ω = 1.0
-
 abstract type Basis end
 struct Oscillator <: Basis
-    Ω::Float64
+    ω::Float64
     n::Int64
 end
-Oscillator() = Oscillator(1.0,10)
-b1 = Oscillator()
+Oscillator(n::Int64) = Oscillator(1.0,n)
 
+b1 = Oscillator(10)
 
-# Oscillator(n) = Oscillator(1.,n)
-# Oscillator() = Oscillator(10)
-# osc = Oscillator()
-# Oscillator(n::Vector) = Oscillator.(n)
-# Oscillator(n::UnitRange{Int64}) = Oscillator.(n)
+index(b::Oscillator) = 1:b.n
+spectrum(b::Oscillator) = (0:b.n-1)*b.ω
+qnumbers(b::Oscillator) = 0:b.n-1
 
-# n = 0:10
-
-# Oscillator.((10,20))
-
-# function eigenstatematrix(basis::Oscillator{1},x)
-    # @unpack_Oscillator basis
-    # M = N[1];  Ω = ω[1]
-# @assert M >= 1
-# M > 370 && error("Float64 Quadrature does not converge for M > 370.")
-
-
-function (t::Oscillator)(x)
-    @unpack Ω,n = t
-    m = 0:n - 1 |> Vector
+function (b::Oscillator)(x)
+    @unpack ω,n = b
     T = x * ones(1, n) |> zero
-    T[:,1] = @. exp(-(√Ω * x)^2 / 2) * (Ω / π)^(1 / 4)
-    n >= 2 && (T[:,2] = @. sqrt(2) * exp(-(√Ω * x)^2 / 2) * (√Ω * x) * (Ω / π)^(1 / 4))
-    for j = 1:n - 2
-        T[:,j + 2] = @. sqrt(2 / (m[j + 2])) * (√Ω * x) * T[:,j + 1] - sqrt(m[j + 1] / m[j + 2]) * T[:,j]
+    T[:,1] = @. exp(-(√ω * x)^2 / 2) * (ω / π)^(1 / 4)
+    n >= 2 && (T[:,2] = @. sqrt(2) * exp(-(√ω * x)^2 / 2) * (√ω * x) * (ω / π)^(1 / 4))
+    for j ∈ index(b)[3:end]
+        m = qnumbers(b)[j]
+        T[:,j] = @. sqrt(2 / m) * (√ω * x) * T[:,j-1] - sqrt((m-1) / m) * T[:,j-2]
     end
     return T
 end
 
-b1 = Oscillator(1.,20)
-T = b1(x)
-c = randn(20)
-f = T*c
 
-@test sum(@. w*exp(x^2)*abs2(f)) ≈ sum(abs2.(c))
 
-#something like this?
+n = 12
+b1 = Oscillator(1.,n)
+plot(x,b1(x),legend=false,grid=false)
+
+
+
+# CField:
+
+#= something like this?
 function transform(b::Oscillator)
     x,w = gausshermite(b.n)
     T = b(x)
     return x,w,T
 end
+
+xk,wk,Tk = transform(b1)
+c = randn(n)
+f = Tk*c
+@test sum(@. wk*exp(xk^2)*abs2(f)) ≈ sum(abs2.(c))
+
+abstract type CField end
+struct YField{N} <: CField
+    basis::Basis
+    psi::Array{Complex{Float64},N}
+end
+
+f1 = YField(b1,randn(b1.n)+im*randn(b1.n))
+
+=#
