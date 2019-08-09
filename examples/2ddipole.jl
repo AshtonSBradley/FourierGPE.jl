4032597af05b1e4e00e06db57c35f4ed848b5f12
AshtonSBradley <abradley@higgs.local>
2019-08-09 13:32:50 +1200

    update energy plot

--- examples/2ddipole.jl
+++ examples/2ddipole.jl
@@ -1,4 +1,4 @@
-using Test, Plots, LaTeXStrings, Revise, FourierGPE
+using Test, Plots, LaTeXStrings, Revise, FourierGPE, VortexDistributions
 gr(titlefontsize=12,size=(500,300),transpose=true,colorbar=false)
 
 function psimovie(sol,sim)
@@ -37,9 +37,10 @@ healinglength(x,y,μ,g) = 1/sqrt(g*abs2(ψ0(x,y,μ,g)))
 
 # ==== make initial state
 x,y = X
+kx,ky = K
 ψi = ψ0.(x,y',μ,g)
 ϕi = kspace(ψi,sim)
-@pack! sim = ϕi,μ
+@pack_Sim! sim
 
 # ====== evolve
 sol = runsim(sim)
@@ -50,23 +51,22 @@ sol = runsim(sim)
 showpsi(x,y,ψg)
 
 # ==== initial dipole
-using VortexDistributions
-
 ψv = copy(ψg)
 d = 30
 ξv = healinglength(0.,0.,μ,g)
-# can use default healing length = 1
+# can use default ξ = 1
 pv = PointVortex(0.,d/2,1)
 nv = PointVortex(0,-d/2,-1)
 
 dipole = [nv;pv]
-psi = Torus(ψv,x,y)
-vortex!(psi,dipole)
-ψv = psi.ψ
+psi = Torus(ψv,x,y) # set field topology for VortexDistributions
+vortex!(psi,dipole) # make dipole
+ψv = psi.ψ # pull out the field
+showpsi(x,y,ψv)
 
 # ==== set simulation parameters
 c = sqrt(μ)
-tf = L[1]/c/2
+tf = L[1]/c/4
 γ = 0.1
 
 t = LinRange(ti,tf,Nt)
@@ -76,7 +76,7 @@ alg = Vern7()
 
 @pack_Sim! sim
 
-# ==== evolve
+# ==== remove boundary artifacts with small γ
 solv = runsim(sim)
 
 # ==== plot
@@ -126,51 +126,83 @@ end
 
 gif(anim,"./examples/dipoleenergies.gif",fps=30)
 
-# check energy conservation
+# ==== energy totals
 
-# nonlinearity
-function xenergy(ϕ,sim,t)
+function venergy(ϕ,sim,t)
     @unpack g,X = sim; x,y = X
     ψ = xspace(ϕ,sim)
-    @. ψ *= 0.5*g*abs2(ψ) + V(x,y',t)
+    @. ψ *= V(x,y',t)
     return kspace(ψ,sim)
 end
 
-function gpenergy(ϕ,sim,t)
-    @unpack μ,γ,K,espec = sim; kx,ky=K
-    dkx,dky = kx[2]-kx[1],ky[2]-ky[1]
-    chi = xenergy(ϕ,sim,t)
-    H = @. espec*abs2(ϕ) + conj(ϕ)*chi
-    return sum(H)*dkx*dky |> real
+function ienergy(ϕ,sim)
+    @unpack g,X = sim; x,y = X
+    ψ = xspace(ϕ,sim)
+    @. ψ *= 0.5*g*abs2(ψ)
+    return kspace(ψ,sim)
 end
 
-H = zero(t)
-Ei = zero(t)
-Ec = zero(t)
-Natoms = zero(t)
-dx,dy = diff(x)[1],diff(y)[1]
+function kinetic(ϕ,sim)
+    @unpack espec,K = sim; kx,ky = K
+    dkx,dky = kx[2]-kx[1],ky[2]-ky[1]
+    return sum(espec.*abs2.(ϕ))*dkx*dky |> real
+end
 
-for (i,t) in enumerate(t)
-    ϕ = solv[i]
+dx = diff(x)[1]; dy = diff(y)[1]
+dkx = diff(kx)[1]; dky = diff(ky)[1]
+n0 = μ/g
+
+function energies(sol)
+    t = sol.t
+    # H = zero(t)
+    Ekall = zero(t)
+    Ekhy = zero(t)
+    Ei = zero(t)
+    Ec = zero(t)
+    Ev = zero(t)
+    Eint = zero(t)
+    Eqp = zero(t)
+    Et = zero(t)
+    Natoms = zero(t)
+
+    for (i,t) in enumerate(t)
+    ϕ = sol[i]
     ψ = xspace(ϕ,sim)
     psi = XField(ψ,X,K,K2)
-    et,ei,ec = energydecomp(psi)
+    ek,ei,ec = energydecomp(psi)
+    Ekhy[i] = sum(ek)*dx*dy |> real
     Ei[i] = sum(ei)*dx*dy |> real
     Ec[i] = sum(ec)*dx*dy |> real
+    Ekall[i] = kinetic(ϕ,sim)
+
+    Ev[i] = sum(@. V(x,y',t)*abs2(ψ))*dx*dy |> real
+    Eint[i] = sum(@. g/2*(abs2(ψ) - n0)^2)*dx*dy |> real
+    Eqp[i] = Ekall[i] - Ekhy[i]
+
+    Et[i] = Ekall[i] + Ev[i] + Eint[i]
     Natoms[i] = sum(abs2.(ψ))*dx*dy
-    H[i] = gpenergy(ϕ,sim,t)
+    end
+    return Ei,Ec,Ek,Eqp,Ev,Eint,Et,Ekall,Natoms
 end
 
-plot(t,Ei./Natoms,label=L"E_i",legend=:bottomright)
-plot!(t,Ec./Natoms,label=L"E_c")
+@time Ei,Ec,Ekhy,Eqp,Ev,Eint,Et,Ekall,Natoms = energies(solh)
+
+# ==== plot energies
+# ==== simple breakdown
+plot(t,Et./Natoms,label=L"E_t",legend=:left)
+plot!(t,Eint./Natoms,label=L"E_{int}")
+plot!(t,Ekall./Natoms,label=L"E_{kall}")
 xlabel!(L"t")
-plot!(t,H./Natoms,label=L"H")
 
-
-relerror(x,x0) = (x - x0)/x0 |> abs
-
-plot(t,relerror.(H,H[1]),label = L"relerr(H)",legend=:topleft)
-plot!(t,relerror.(Natoms,Natoms[1]),label = L"relerr(N)")
+# ==== decomposition
+plot(t,(Ekall .+ Eint)./Natoms,label=L"E_{kall}+E_{int}",w=5,c=:gray,alpha=0.3)
+plot!(t,Et./Natoms,label=L"E_t",legend=:left)
+plot!(t,Ei./Natoms,label=L"E_k^i")
+plot!(t,Ec./Natoms,label=L"E_k^c")
+plot!(t,Eint./Natoms,label=L"E_{int}")
+plot!(t,Eqp./Natoms,label=L"E_{qp}")
+plot!(t,Ekall./Natoms,label=L"E_{kall}")
+xlabel!(L"t")
 
 
 # if we need to save data:
