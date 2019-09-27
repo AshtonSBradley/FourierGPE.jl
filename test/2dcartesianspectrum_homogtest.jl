153972352cbda4f56ad0f2aa635daa38aa26d895
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2019-09-27 21:58:30 +1200

    minimal example for dipole spectrum

--- test/2dcartesianspectrum_homogtest.jl
+++ test/2dcartesianspectrum_homogtest.jl
@@ -1,7 +1,5 @@
-using Test, SpecialFunctions, VortexDistributions
-using LazyArrays, FillArrays
+using VortexDistributions
 using Revise, FourierGPE
-
 
 #--- Initialize simulation
 # units of healing length, chemical potential
@@ -10,77 +8,47 @@ N = (512,512)
 sim = Sim(L,N)
 @unpack_Sim sim
 
-#--- set simulation parameters
+#--- homogeneous state
 μ = 1.0
 g = 0.01
-γ = 0.5
-ti = 0.0
-tf = .3pi
-Nt = 150
-t = LinRange(ti,tf,Nt)
-
-#--- Useful state functions
 ψ0(x,y,μ,g) = sqrt(μ/g)*sqrt(max(1.0-V(x,y,0.0)/μ,0.0)+im*0.0)
 healinglength(x,y,μ,g) = 1/sqrt(g*abs2(ψ0(x,y,μ,g)))
-
-#--- Make initial state
 x,y = X
 kx,ky = K
-ψi = ψ0.(x,y',μ,g)
-ϕi = kspace(ψi,sim)
-@pack_Sim! sim
-
-# evolve
-sol = runsim(sim)
-
-#--- pull out the ground state
-ϕg = sol[end]
-ψg = xspace(ϕg,sim)
+ψg = ψ0.(x,y',μ,g)
 showpsi(x,y,ψg)
 
-#--- periodic dipole phase
-# Billam et al, PRL 112, 145301 (2014), Supplemental
-H(x) = x > 0. ? 1.0 : 0.0
-shift(x,xi) = x - xi
-tans(x,xk) = tan((shift(x,xk) - π)*0.5)
-tanhs(x,xk,j) = tanh((shift(x,xk) + 2*π*j)*0.5)
-
-function kernel(x,y,xp,yp,xn,yn,j)
-    return atan(tanhs(y,yn,j)*tans(x,xn)) -
-    atan(tanhs(y,yp,j)*tans(x,xp))
-end
-
-# Dimensionless form
-function θd(x,y,dip)
-    vp,vn = dip
-    xp,yp,_ = rawData(vp)
-    xn,yn,_ = rawData(vn)
-    s = 0.0
-    for j = -5:5
-        s += kernel(x,y,xp,yp,xn,yn,j)
-    end
-    return s + π*(H(shift(x,xp)) - H(shift(x,xn))) - y*(xp - xn)/(2*π)
-end
-
-# arbitrary domains and dipole sizes:
-function thetad(x,y,xp,yp,xn,yn)
-    s = 0.0
-    for j = -5:5
-        s += kernel(x,y,xp,yp,xn,yn,j)
-    end
-    s += π*(H(shift(x,xp)) - H(shift(x,xn))) - y*(xp - xn)/(2*π)
-    return s - x*H(abs(yp - yn) - π) + y*H(abs(xp - xn) - π)
-end
-
-function Thetad(x,y,xp,yp,xn,yn)
-    Lx = x[end]-x[1]
-    Ly = y[end]-y[1]
-    return @. angle(exp(im*thetad.(x*2*pi/Lx,y'*2*pi/Ly,xp*2*pi/Lx,yp*2*pi/Ly,xn*2*pi/Lx,yn*2*pi/Ly)))
-end
+# #--- periodic dipole phase
+# # Billam et al, PRL 112, 145301 (2014), Supplemental
+# H(x) = x > 0. ? 1.0 : 0.0
+# shift(x,xi) = x - xi
+# tans(x,xk) = tan((shift(x,xk) - π)*0.5)
+# tanhs(x,xk,j) = tanh((shift(x,xk) + 2*π*j)*0.5)
+#
+# function kernel(x,y,xp,yp,xn,yn,j)
+#     return atan(tanhs(y,yn,j)*tans(x,xn)) -
+#     atan(tanhs(y,yp,j)*tans(x,xp))
+# end
+#
+# # arbitrary domains and dipole sizes:
+# function thetad(x,y,xp,yp,xn,yn)
+#     s = 0.0
+#     for j = -5:5
+#         s += kernel(x,y,xp,yp,xn,yn,j)
+#     end
+#     s += π*(H(shift(x,xp)) - H(shift(x,xn))) - y*(xp - xn)/(2*π)
+#     return s - x*H(abs(yp - yn) - π) + y*H(abs(xp - xn) - π)
+# end
+#
+# function Thetad(x,y,xp,yp,xn,yn)
+#     Lx = x[end]-x[1]
+#     Ly = y[end]-y[1]
+#     return @. thetad.(x*2*pi/Lx,y'*2*pi/Ly,xp*2*pi/Lx,yp*2*pi/Ly,xn*2*pi/Lx,yn*2*pi/Ly)
+# end
 
 #--- initial dipole, with periodic phase
-ψv = copy(ψg)
-d = 14
+ψd = copy(ψg)
+d = 12
 ξv = healinglength(0.,0.,μ,g)
 # can use default ξ = 1
 xp,yp = 0.,d/2
@@ -89,52 +57,27 @@ pv = PointVortex(xp,yp,1)
 nv = PointVortex(xn,yn,-1)
 dipole = [nv;pv]
 
-psi = Torus(ψv,x,y) # set field topology for VortexDistributions
+psi = Torus(ψd,x,y) # set field topology for VortexDistributions
 vortex!(psi,dipole) # make dipole
-ψv = abs.(psi.ψ).*exp.(im*Thetad(x,y,xp,yp,xn,yn))
-showpsi(x,y,ψv)
-
-#--- evolve dipole with weak damping
-# Set simulation parameters
-c = sqrt(μ)
-tf = L[1]/c # a short run for testing evolution and detection
-γ = 0.03
-
-t = LinRange(ti,tf,Nt)
-ϕi = kspace(ψv,sim)
-alg = Vern7()
-
-@pack_Sim! sim
-
-# remove boundary artifacts with small γ
-@time solv = runsim(sim)
-
-ψd = xspace(solv[end],sim)
+ψd = abs.(psi.ψ).*exp.(im*Thetad(x,y,xp,yp,xn,yn))
 showpsi(x,y,ψd)
 
 #--- test new methods
-kL = 0.1*2*pi/L[1]
-kξ =   2*pi
-kmin = kL
+kL = 2*pi/L[1]
+kξ = 2*pi
 
+kmin = 0.1*kL
 kmax = kξ
+
 Np = 200
-kp = log10range(kmin,kmax,Np)
+k = log10range(kmin,kmax,Np)
 
 Ek = kespectrum(kp,ψi,X,K)
-plot(kp,Ek,scale=:log10)
+plot(k,Ek,scale=:log10)
 
 # heatmap(log.(abs2.(A |> fftshift) .+ eps.()))
 
-#--- test incompressible spectrum
-
-
-
-
-#--- convolutions
-# measures for zero padded fields
-
-
+#--- incompressible spectrum
 kL = 2*pi/L[1]
 kξ = 2*pi
 kd = 2*pi/d
@@ -142,16 +85,16 @@ kd = 2*pi/d
 kmin = 0.1*kL
 kmax = 1.3kξ
 Np = 400
-kp = log10range(kmin,kmax,Np)
+k = log10range(kmin,kmax,Np)
 
-Eki = ikespectrum(kp,ψd,X,K)
+Eki = ikespectrum(k,ψd,X,K)
 
 #--- power-law plot in logspace
 n0 = abs2.(ψd[1,1])
 E0 = π*n0
-p1 = plot(kp,Eki/E0,scale=:log10,grid=false,label=L"E_i(k)",legend=:bottomleft)
-plot!(kp,2kp.^(-3),label=L"k^{-3}")
-plot!(kp,200*kp,label=L"k")
+p1 = plot(k,Eki/E0,scale=:log10,grid=false,label=L"E_i(k)",legend=:bottomleft)
+plot!(k,2k.^(-3),label=L"k^{-3}")
+plot!(k,200*k,label=L"k")
 ylims!(5e-4,1e2)
 # xlims!(0.02,10)
 vline!([kL],ls=:dash,label=L"k_L")
@@ -160,8 +103,8 @@ vline!([kξ],ls=:dash,label=L"k_\xi")
 xlabel!(L"k\xi")
 ylabel!(L"E_i(k)")
 
-#--- test against analytic form
-# use the numerical dipole distance since system has evolved
+# test against analytic form
+# use the numerical dipole distance
 vort = findvortices(Torus(ψd,x,y)) |> rawData
 d = vort[:,2] |> diff
 Λ = 0.8249
@@ -169,4 +112,4 @@ f(x) = x*(besselk(1,x)*besseli(0,x)-besselk(0,x)*besseli(1,x))
 F(x,Λ) = f(x/(2Λ))^2/x
 F(x) = F(x,Λ)
 Ed(k,d) = 2*F(k)*(1-besselj0(k*d))
-plot!(kp,Ed.(kp,d),label=L"{\cal E}_i^a(k)")
+plot!(k,Ed.(k,d),label=L"{\cal E}_i^a(k)")
