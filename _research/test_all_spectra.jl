8481906f2703d1c9e35f046184c8d425acf244f5
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-06-25 21:14:53 +1200

    total ke?

--- _research/test_all_spectra.jl
+++ _research/test_all_spectra.jl
@@ -51,9 +51,8 @@ function ikespectrum(k,ψ,X,K)
     ρ = hypot.(xp,yp')
 
     Eki = zero(k)
-    for i in eachindex(k)
-        κ = k[i]
-        Eki[i]  = 0.5*κ*sum(@. besselj0(κ*ρ)*Ci)*dx*dy |> real
+    for (j,kj) in enumerate(k)
+        Eki[j]  = 0.5*kj*sum(@. besselj0(kj*ρ)*Ci)*dx*dy |> real
     end
     return Eki
 end
@@ -64,40 +63,72 @@ Eki = ikespectrum(k,ψi,X,K)
 ## plot
 plot(k,Eki)
 
-##
-ψi
-
 ## kespectrum fix
 function kespectrum(k,ψ,X,K)
     x,y = X; kx,ky = K
     dx,dy = diff(x)[1],diff(y)[1]
-    DX,DK = dfftall(X,K)
-    k2field = k2(K)
-    psi = XField(ψ,X,K,k2field)
-    vx,vy = velocity(psi)
-    rho = abs2.(ψ)
-    wx = @. sqrt(rho)*vx; wy = @. sqrt(rho)*vy
+    psiac = autocorrelate(ψ,X,K,periodic=true)
 
-    cwx = autocorrelate(wx,X,K)
-    cwy = autocorrelate(wy,X,K)
-    Ci = cwx .+ cwy
-
+    # make ρ
     Nx = 2*length(x)
     Lx = x[end]-x[1] + dx
     xp = LinRange(-Lx,Lx,Nx+1)[1:Nx]
     yp = xp
     ρ = hypot.(xp,yp')
 
-    Eki = zero(k)
-    for i in eachindex(k)
-        κ = k[i]
-        Eki[i]  = 0.5*κ*sum(@. besselj0(κ*ρ)*Ci)*dx*dy |> real
+    # do spectra
+    Ek = zero(k)
+    for (i,ki) in enumerate(k)
+        Ek[i]  = 0.5*ki^3*sum(@. besselj0(ki*ρ)*psiac)*dx*dy |> real
     end
-    return Eki
+    return Ek
 end
 
 ## call
+k = LinRange(0.01,10.0,100) |> collect
 Ek = kespectrum(k,ψi,X,K)
 
 ## plot
 plot(k,Ek)
+
+## make autocorrelate periodic
+using FFTW
+import FourierGPE:autocorrelate
+
+@doc raw"""
+	autocorrelate(ψ,X,K,periodic=false)
+
+Return the autocorrelation integral of a complex field ``\psi``, ``A``, given by
+
+```
+A(\rho)=\int d^2r\;\psi^*(r-\rho)\psi(r)
+```
+
+defined on a cartesian grid on a cartesian grid using FFTW.
+
+`X` and `K` are tuples of vectors `x`,`y`,`kx`, `ky`.
+
+This method is useful for evaluating spectra from cartesian data.
+"""
+function autocorrelate(ψ,X,K;periodic=false)
+    DX,DK = dfftall(X,K)
+    if periodic == false
+        ϕ = zeropad(ψ)
+    else
+        ϕ = ψ
+    end
+	χ = fft(ϕ)*prod(DX)
+	return ifft(abs2.(χ))*prod(DK) |> fftshift
+end
+
+## what does autocorrelate look like?
+x,y = X; kx,ky = K
+dx,dy = diff(x)[1],diff(y)[1]
+psiac = autocorrelate(ψi,X,K)
+psiac2 = autocorrelate(ψi,X,K,periodic=true)
+
+## plot
+heatmap(abs2.(psiac2))
+
+##
+psiac2
