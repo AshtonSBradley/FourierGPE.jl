78912ef97a8d8af229e9e226f901eb0f827f20cb
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-06-27 10:42:31 +1200

    tidy up test_all_spectra

--- _research/test_all_spectra.jl
+++ _research/test_all_spectra.jl
@@ -18,37 +18,6 @@ dx,dy = diff(x)[1],diff(y)[1]
 ## plot
 heatmap(x,y,abs2.(ψi))
 
-## ike spectrum
-import FourierGPE:ikespectrum, kespectrum
-
-function ikespectrum(k,ψ,X,K;periodic=false)
-    x,y = X; kx,ky = K
- 	dx,dy = diff(x)[1],diff(y)[1]
-	DX,DK = dfftall(X,K)
-    k2field = k2(K)
-    psi = XField(ψ,X,K,k2field)
-    vx,vy = velocity(psi)
-    rho = abs2.(ψ)
-    wx = @. sqrt(rho)*vx; wy = @. sqrt(rho)*vy
-    Wi, Wc = helmholtz(wx,wy,psi)
-    wix,wiy = Wi
-
-	cwix = autocorrelate(wix,X,K,periodic=periodic)
-	cwiy = autocorrelate(wiy,X,K,periodic=periodic)
-    Ci = cwix .+ cwiy
-
-    Nx = 2*length(x)
-    Lx = x[end]-x[1] + dx
-    xp = LinRange(-Lx,Lx,Nx+1)[1:Nx]
-    yp = xp
-    ρ = hypot.(xp,yp')
-
-    Eki = zero(k)
-    for (j,kj) in enumerate(k)
-        Eki[j]  = 0.5*kj*sum(@. besselj0(kj*ρ)*Ci)*dx*dy |> real
-    end
-    return Eki
-end
 
 ## call
 Eki = ikespectrum(k,ψi,X,K)
@@ -56,33 +25,7 @@ Eki = ikespectrum(k,ψi,X,K)
 ## plot
 plot(k,Eki)
 
-## kespectrum fix
-function kespectrum(k,ψ,X,K;periodic=false)
-    x,y = X; kx,ky = K
-    dx,dy = diff(x)[1],diff(y)[1]
-    DX,DK = dfftall(X,K)
-    k2field = k2(K)
-    psi = XField(ψ,X,K,k2field)
-    ψx,ψy = gradient(psi)
 
-	cx = autocorrelate(ψx,X,K,periodic=periodic)
-	cy = autocorrelate(ψy,X,K,periodic=periodic)
-    Ci = cx .+ cy
-
-    # make ρ
-    Nx = 2*length(x)
-    Lx = x[end]-x[1] + dx
-    xp = LinRange(-Lx,Lx,Nx+1)[1:Nx]
-    yp = xp
-    ρ = hypot.(xp,yp')
-
-    # do spectra
-    Ek = zero(k)
-    for (j,kj) in enumerate(k)
-        Ek[j]  = 0.5*kj*sum(@. besselj0(kj*ρ)*Ci)*dx*dy |> real
-    end
-    return Ek
-end
 
 ## call
 k = LinRange(0.01,10.0,100) |> collect
@@ -91,35 +34,7 @@ Ek = kespectrum(k,ψi,X,K)
 ## plot
 plot(k,Ek)
 
-## make autocorrelate periodic
-using FFTW
-import FourierGPE:autocorrelate
 
-@doc raw"""
-	autocorrelate(ψ,X,K,periodic=false)
-
-Return the autocorrelation integral of a complex field ``\psi``, ``A``, given by
-
-```
-A(\rho)=\int d^2r\;\psi^*(r-\rho)\psi(r)
-```
-
-defined on a cartesian grid on a cartesian grid using FFTW.
-
-`X` and `K` are tuples of vectors `x`,`y`,`kx`, `ky`.
-
-This method is useful for evaluating spectra from cartesian data.
-"""
-function autocorrelate(ψ,X,K;periodic=false)
-    DX,DK = dfftall(X,K)
-    if periodic == false
-        ϕ = zeropad(ψ)
-    else
-        ϕ = ψ
-    end
-	χ = fft(ϕ)*prod(DX)
-	return ifft(abs2.(χ))*prod(DK) |> fftshift
-end
 
 ## what does autocorrelate look like?
 x,y = X; kx,ky = K
