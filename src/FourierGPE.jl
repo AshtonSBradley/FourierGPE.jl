dce7de68fee8bc0e4c8c311bd5e4f595be382fca
AshtonSBradley <ashton.bradley@gmail.com>
2020-08-10 10:20:46 +1200

    fix export bessel_projection

--- src/FourierGPE.jl
+++ src/FourierGPE.jl
@@ -24,7 +24,7 @@ include("evolution.jl")
 include("analysis.jl")
 
 export Simulation, TransformLibrary, UserParams
-export xvec, kvec, xvecs, kvecs, dfft, dfftall, crandn_array, crandnpartition
+export xvec, kvec, xvecs, kvecs, dfft, dfftall, crandn_array, crandnpartition 
 export maketransarrays, makearrays, xspace, xspace!, kspace, kspace!
 export nlin, nlin!, Lgp, Lgp!, V, Params
 export initsim!, runsim, internalnorm
@@ -34,6 +34,6 @@ export showpsi, c1, c2, c3
 export Params, @pack_Params!, @unpack_Params
 export k2, @pack!, @unpack, makeT, definetransforms #makeTMixed,
 export Field, XField, KField, gradient, velocity, current
-export energydecomp, helmholtz, kespectrum, ikespectrum, ckespectrum, qpespectrum, bessel_projection,
+export energydecomp, helmholtz, kespectrum, ikespectrum, ckespectrum, qpespectrum, bessel_projection 
 export log10range, zeropad, autocorrelate, convolve
 end # module
