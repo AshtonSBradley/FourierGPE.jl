972b6e764af224096be580e2ef7e75c4b6f09c48
Ashton Bradley <ashton.bradley@gmail.com>
2020-10-19 22:34:06 +1300

    Update build.jl
    make mkl default

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,4 @@
-## For faster 1D and 2D evalution, recompile with MKL by 
-# 1. uncomment the ENV line below. 
-# 2. ] build FourierGPE
-#
-# Note: this will break 3D, as MKL does not support all of FFTW
-
-# ENV["JULIA_FFTW_PROVIDER"]="MKL"
+# MKL now supports 3D transforms, so...
+ENV["JULIA_FFTW_PROVIDER"]="MKL"
 
 
