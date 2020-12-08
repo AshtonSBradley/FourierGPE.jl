cfe38d365df6778a166aa08a6f83fa60e7031ade
Ashton Bradley <ashton.bradley@gmail.com>
2020-12-09 09:03:15 +1300

    kvecs now use fftfreq

--- src/arrays.jl
+++ src/arrays.jl
@@ -4,27 +4,27 @@ linspace(a,b,n) = LinRange(a,b,n) |> collect
 """
     x = xvec(λ,N)
 
-Creates `x` values with correct periodicity for box specified by length `λ` for number of points `N`.
+Create `x` values with correct periodicity for box specified by length `λ`, using `N` points.
 """
 xvec(L,N) = LinRange(-L/2,L/2,N+1)[2:end] |> collect
 
 """
     k = kvec(λ,N)
 
-Creates `k` values with correct periodicity for box specified by length `λ` for number of points `N`.
+Create `k` values with correct periodicity for box specified by length `λ` for number of points `N`.
 """
 function kvec(λ,N)
-    @assert iseven(N)
-    nk = 0:Int(N/2)
-    k = [nk[1:end-1];-reverse(nk[2:end])]*2*π/λ
-    # k = fftfreq(N)*N*2*π    # N not restricted
+    # @assert iseven(N)
+    # nk = 0:Int(N/2)
+    # k = [nk[1:end-1];-reverse(nk[2:end])]*2*π/λ
+    k = fftfreq(N)*N*2*π/λ    # N not restricted
     return k
 end
 
 """
     X = xvecs(L,N)
 
-Creates a tuple containing the spatial coordinate array for each spatial dimension.
+Create a tuple containing the spatial coordinate array for each spatial dimension.
 """
 function xvecs(L,N)
     X = []
@@ -38,7 +38,7 @@ end
 """
     K = kvecs(L,N)
 
-Creates a tuple containing the spatial coordinate array for each spatial dimension.
+Create a tuple containing the spatial coordinate array for each spatial dimension.
 """
 function kvecs(L,N)
     K = []
@@ -52,18 +52,18 @@ end
 """
     k² = k2(K)
 
-Creates the kinetic energy array `k²` on the `k`-grid defined by the tuple `K`.
+Create the kinetic energy array `k²` on the `k`-grid defined by the tuple `K`.
 """
 function k2(K)
     kind = Iterators.product(K...)
-    return map(x-> sum(abs2.(x)),kind)
+    return map(k-> sum(abs2.(k)),kind)
 end
 
 """
     X,K,dX,dK = makearrays(L,N)
 
-Creates all `x` and `k` arrays for box specified by tuples `L=(Lx,...)` and `N=(Nx,...)`.
-Differenetials `dX`, `dK` are also reaturned. `L` and `N` must be tuples of equal length.
+Create all `x` and `k` arrays for box specified by tuples `L=(Lx,...)` and `N=(Nx,...)`.
+For convenience, differentials `dX`, `dK` are also reaturned. `L` and `N` must be tuples of equal length.
 """
 function makearrays(L,N)
     @assert length(L) == length(N)
