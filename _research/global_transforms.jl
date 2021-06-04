9e3aa60911d969b6333e2bc6c6668bebccaf8d7c
Ashton Bradley <ashton.bradley@gmail.com>
2021-06-04 20:15:23 +1200

    global transforms lib wip

---
+++ _research/global_transforms.jl
@@ -0,0 +1,25 @@
+#build global const transform lib as in:
+# https://github.com/JuliaLang/julia/blob/8535b8c4d7f4179f7b13a04f16074a2446dda3ac/stdlib/Random/src/RNGs.jl#L364-L366
+# struct _GLOBAL_RNG <: AbstractRNG
+#     global const GLOBAL_RNG = _GLOBAL_RNG.instance
+# end
+
+# function __init__()
+#     TransformLib()
+# end
+
+abstract type AbstractLib end
+
+struct TransformLib <: AbstractLib 
+    t1::Matrix{Float64}
+    t2::Matrix{Float64}
+    
+    TransformLib(t1,t2) = new(t1,t2)
+    TransformLib() = TransformLib(rand([0 1],2,2),rand([0 1],2,2))
+    global const GLOBAL_TRANSFORM_LIB = TransformLib()
+end
+
+##
+
+__init__()
+
