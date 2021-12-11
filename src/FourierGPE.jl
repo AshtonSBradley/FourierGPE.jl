a7265bb621c6da28a3ae53b5c909926bc03b8c89
Ashton <ashton.bradley@gmail.com>
2021-12-11 16:39:40 +1300

    tests pass

--- src/FourierGPE.jl
+++ src/FourierGPE.jl
@@ -4,24 +4,24 @@ using Reexport
 using SpecialFunctions
 using LazyArrays
 using FillArrays
-using DifferentialEquations
+using OrdinaryDiffEq
 using LoopVectorization    
 using Tullio               
 using RecursiveArrayTools
 using PaddedViews
-using LaTeXStrings
-using ColorSchemes
-
 using FFTW
-using Plots
+import DiffEqCallbacks.FunctionCallingCallback
+
+# using LaTeXStrings
+# using ColorSchemes
+# using Plots
+# const c1 = cgrad(ColorSchemes.inferno.colors)
+# const c2 = cgrad(ColorSchemes.RdBu_11.colors)
+# const c3 =:mediumseagreen
+
 @reexport using Parameters
 @reexport using JLD2
 @reexport using FileIO
-
-
-const c1 = cgrad(ColorSchemes.inferno.colors)
-const c2 = cgrad(ColorSchemes.RdBu_11.colors)
-const c3 =:mediumseagreen
 
 include("types.jl")
 include("arrays.jl")
@@ -36,7 +36,7 @@ export nlin, nlin!, Lgp, Lgp!, V, Params
 export initsim!, runsim, internalnorm
 export Transforms, @pack_Transforms!, @unpack_Transforms
 export Sim, @pack_Sim!, @unpack_Sim, testsim
-export showpsi, c1, c2, c3
+# export showpsi, c1, c2, c3
 export Params, @pack_Params!, @unpack_Params
 export k2, @pack!, @unpack, makeT, definetransforms #makeTMixed,
 export Field, XField, KField
